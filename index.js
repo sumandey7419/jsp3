3d8484634396b832e55cfc8dbbbd95fd11566396
sumandey7419 <sumandey74192.gmail.com>
2022-10-07 16:33:52 +0530

    msg

---
+++ index.js
@@ -0,0 +1,21 @@
+// var total=0;
+// var count=0;
+// while(count<5)
+// {
+// var marks=prompt("enter your marks,")
+// document.write("the marks is"+marks+"<br>")
+// total=total+marks;
+// count++;
+// }
+// document.write("total marks is"+total)
+
+// let total_number=[5];
+// for(var i=0;i<5;i++)
+// {
+// total_number[i]=prompt("enter your marks,");
+// }
+// for(var j=0;j<5;j++)
+// {
+// document.write("the marks is"+total_number[j]+"<br>")
+// }
+
---
+++ index1.js
@@ -0,0 +1,40 @@
+//For Loop
+
+// let val = 0;
+// let arr2 = [];
+
+// for(val = 0; val<5; val++){
+//     ip = prompt("Enter the Marks: ");
+//     arr2[val] = ip;
+
+// }
+// document.write("<br>"+arr2)
+
+//While Loop
+// let count = 0;
+// let arr = [];
+
+// while(count<5){
+//     ip = prompt("Enter the Marks: ");
+//     arr[count] = ip;
+//     count++;
+
+// }
+// document.write(arr)
+
+
+
+
+//While Loop
+
+// let count = 0;
+// let total = 0;
+// let arr = []
+
+// while(count<5){
+// arr[count]=parseInt(prompt("Enter the Value:"))
+// total = parseInt(total+arr[count])
+// count++
+// }
+// document.write(arr)
+// document.write("<br>"+total)
